d791cbcb3c0561d7b138b0088bfcec8d84a5be85
abhishekmalakar <malakar.abh@gmail.com>
2023-01-08 10:26:13 +0530

    Changes Made

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
+
+    int fib(int end){
+        int a=1;
+        int b=1;
+
+        int sum=a+b;
+
+        return end;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
     }
 }
